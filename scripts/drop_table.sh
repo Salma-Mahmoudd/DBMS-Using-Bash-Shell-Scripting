c588d351be9b469905ccc8e8ef5b25129873b1e1
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2024-12-11 20:59:00 +0200

    Write the script as a function

--- scripts/drop_table.sh
+++ scripts/drop_table.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-if [[ -n "$1" && -f "$1" ]]; then
-    rm "$1"
-else
-    exit 1
-fi
+# drop_table: remove table from the database
+# $1: table name
+#
+# Return: 0 if success, 1 if argument does not exist, 2 if table does not exist
+
+function drop_table() {
+    local success=0
+
+    if [[ -z "$1" ]]; then
+        success=1
+    elif [[ ! -f "$1" ]]; then
+        success=2
+    else
+        rm "$1"
+    fi
+
+    return $success
+}
