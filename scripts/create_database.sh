8d6683a170720c5c71f3f1fea88a36f9fd1a70bf
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2024-12-11 22:38:02 +0200

    Write scripts as functions and check for primary key when create a table

--- scripts/create_database.sh
+++ scripts/create_database.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-if [[ -n "$1" && ! -d "$1" && "$1" =~ ^[a-zA-Z0-9._]+$ ]]; then
-    mkdir "$1" && touch ./"$1"/metadata
-else
-    exit 1
-fi
+# create_database: create database folder
+# $1: database name
+#
+# Return: 0 if success, 1 if argument does not exist,
+# 2 if database already exists and 3 if invalid name format
+
+function create_database() {
+    local success=0
+
+    if [[ -z "$1" ]]; then
+        success=1
+    elif [[ -d "$1" ]]; then
+        success=2
+    elif [[ ! "$1" =~ ^[a-zA-Z0-9._]+$ ]]; then
+        success=3
+    else
+        mkdir "$1" && touch ./"$1"/metadata
+    fi
+
+    return $success
+}
--- scripts/create_table.sh
+++ scripts/create_table.sh
@@ -1,42 +1,60 @@
 #!/bin/bash
 
-# create table script
-# input tableName col:datatype
-# 1-> missing arguments
-# 2-> table exists
-# 3-> invalid format or datatype
+# create_table: create table in specific database and its metadata. First column must be a primary key
+# $1: table name
+# $2 $3...: column names and their types (e.g., column1 primary:string, column2:int)
+#
+# Return: 0 if success, 1 if arguments do not exist, 2 if table exists,
+# 3 if invalid syntax and 4 if there is no primary key
 
-shopt -s nocasematch
 
-typeset -i res=0
-if [ $# -lt 2 ]
-then
-    res=1
-else
-    if [ -f $1 ]
+function create_table() {
+    shopt -s nocasematch
+
+    typeset -i res=0
+    typeset -i pk=1
+    str=""
+    str2=""
+    
+    if [ $# -lt 2 ]
     then
-        res=2
+        res=1
     else
-        file=$1
-        str=$1
-        shift
-        while [ $# -ne 0  ]
-        do
-            if [[  $1 =~ ^[a-z0-9_]+:(string|int)$ ]]
-            then
-                str="$str;$1"
-                shift
-            else
-                res=3
-                break
-            fi
-        done
-        if [ $res -eq 0 ]
+        if [ -f "$1" ]
         then
-            echo $str >> metaData
-            touch $file
+            res=2
+        else
+            file=$1
+            str=$1
+            shift
+            while [ $# -ne 0  ]
+            do
+                if [[ $1 =~ ^[a-z0-9_]+:(string|int)$ ]]
+                then
+                    str2="$str2;$1"
+                    shift
+                elif [[ $1 =~ ^[a-z0-9_]+[[:space:]]primary:(string|int)$ && $pk == 1 ]]
+                then
+                    pk=0
+                    str="$str;$(echo "$1" | sed -E 's/^([a-z0-9_]+) primary:(string|int)$/\1:\2/')"
+                    shift
+                else
+                    res=3
+                    break
+                fi
+            done
+            if [[ $pk -ne 0 && $res -eq 0 ]]
+            then
+                res=4
+            elif [ $res -eq 0 ]
+            then
+                str="$str$str2"
+                echo "$str" >> metadata
+                touch "$file"
+            fi
         fi
     fi
-fi
-shopt -u nocasematch
-exit $res
+    shopt -u nocasematch
+
+    return $res
+}
