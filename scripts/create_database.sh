d5974213b12f7f50f834584db903d7c076c9d0df
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2024-12-11 18:14:24 +0200

    Remove - from database name

--- scripts/create_database.sh
+++ scripts/create_database.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ -n "$1" && ! -d "$1" && "$1" =~ ^[a-zA-Z0-9._-]+$ ]]; then
+if [[ -n "$1" && ! -d "$1" && "$1" =~ ^[a-zA-Z0-9._]+$ ]]; then
     mkdir "$1" && touch ./"$1"/metadata
 else
     exit 1
