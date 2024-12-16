cf5ba9bd3b4de2d775a98b39034e92f27b65862d
reem <tarekreem950@gmail.com>
2024-12-16 10:36:12 +0200

    fix

--- scripts/select_from-table.sh
+++ scripts/select_from-table.sh
@@ -9,9 +9,11 @@
 # 0 if success,
 # 1 if arguments do not exist
 # 2 if table does not exist
+# 3 column doesn't exist
 
 select_from_table(){
-    typeset -i res ind
+    typeset -i res=0 ind i=0 arr[2]
+    typeset  columns[2]
 
     if [ $# -lt 2 ]
     then
@@ -34,9 +36,49 @@ select_from_table(){
             for(i=1; i<= NF && (!num || NR <= num); i++) {
                 row = row $i "\t\t"
             };
-            print row
+            if(num && NR <= num || !num) print row;
         }' $1
-        
+    else
+        file=$1
+        limit=$2
+        shift 2
+        metadata=`grep "^$file;" metadata`
+        while (( $# > 0 && $res != 3 ))
+        do
+            result=$(echo $metadata | awk -v column="$1"  -F';' '
+            {   res = 3;
+                for(i=2; i<=NF; i++) {
+                    split($i, arr, ":");
+                    if (arr[1] == column )  {
+                        res = 0;
+                        ind = i - 1;
+                        break;
+                    }
+                }
+            } END{ print res, ind }')
+            read res ind <<< "$result"
+            arr[$i]=$ind
+            columns[$i]=$1
+            ((i++))
+            shift
+        done
+        if [ $res -eq 0 ]
+        then
+            row=""
+            for name in ${columns[@]}
+            do
+                row="$row$name\t\t"
+            done
+            echo -e "$row"
+            awk -F: -v num=$limit  -v arr="${arr[*]}" -v size=${#arr[@]} '{
+                row="";
+                split(arr, indices, " ")
+                for(i=1 ; i <= size && (!num || NR <= num); i++) {
+                    row = row $indices[i] "\t\t"
+                }
+                if(num && NR <= num  || !num) print row;
+            }' $file
+        fi
     fi
     return $res
 }
--- scripts/update_table.sh
+++ scripts/update_table.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # update_table: update table data
 # $1: table name
-# $2: primary key value
+# $2: search column and value
 # $3: column name and it's updated value (e.g., column2:2)
 #
 # Return:
@@ -13,7 +13,8 @@
 # 4 if invalid data type
 
 update_table(){
-    typeset -i res ind
+    typeset -i res=0 ind i=0 arr[2]
+    typeset  columns[2]
 
     if [ $# -lt 3 ]
     then
@@ -23,34 +24,43 @@ update_table(){
         res=2
     else
         metadata=`grep "^$1;" metadata`
-        id=$2
-        column=`echo $3 | cut -d: -f1`
-        value=`echo $3 | cut -d: -f2`
-        result=$(echo $metadata | awk -v column="$column" -v value="$value" -F';' '
-        {   
-            res=3
-            for(i=2; i<=NF; i++) {
-                split($i, arr, ":");
-                if (arr[1] == column )  {
-                    if(arr[2] == "int" && value ~ /^[0-9]+$/ || arr[2] == "string" ){
-                        ind=i-1;
-                        res=0;
-                    }else{
-                        res=4;
-                    };
-                    break;
-                }else{
-                    res=3;
+        file=$1
+        shift
+        while (( $# > 0 && $res == 0 ))
+        do
+            column=`echo $1 | cut -d: -f1`
+            value=`echo $1 | cut -d: -f2`
+            result=$(echo $metadata | awk -v column="$column" -v value="$value" -F';' '
+            {   res = 3;
+                for(i=2; i<=NF; i++) {
+                    split($i, arr, ":");
+                    if (arr[1] == column )  {
+                        if(arr[2] == "int" && value ~ /^[0-9]+$/ || arr[2] == "string" ){
+                         ind=i-1;
+                         res=0;
+                     }else{
+                         res=4;
+                     };
+                     break;
+                    }
                 }
-            }
-        } END{ print res, ind }')
-        read res ind <<< "$result"
+            } END{ print res, ind }')
+            read res ind <<< "$result"
+            values[$i]=$value
+            indices[$i]=$ind
+            ((i++))
+            shift
+        done
         if [ $res -eq 0 ]
         then
-            awk -F':' -v pat="$id" -v ind="$ind" -v val="$value" '{
-                if($0 ~ "^" pat){ $ind = val } ;
-                print 
-            }' OFS=':' $1 > temp.txt && mv temp.txt $1
+           awk -F':' -v keyval="${values[0]}" -v value="${values[1]}" -v key="${indices[0]}" -v ind="${indices[1]}" '
+           {
+                 if ($key == keyval) {
+                     $ind = value;
+                    }
+             print;
+            }' OFS=':' "$file" > temp.txt && mv temp.txt "$file"
+
         fi
     fi
     return $res
